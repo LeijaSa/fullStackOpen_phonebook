0fc615887185307f47544d1707f213e220e79f9d
Leija Salmivirta <leija.salmivirta@gmail.com>
2024-11-18 21:53:29 +0200

    Add ESlint and fix lint errors

--- models/person.js
+++ models/person.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 mongoose
   .connect(url)
   .then((result) => {
-    console.log("connected to MongoDB");
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -21,7 +21,7 @@ const personSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator: function (value) {
-        return /^[\p{L}\s]+$/u.test(value); // Allow Unicode letters and spaces
+        return /^[\p{L}\s]+$/u.test(value) // Allow Unicode letters and spaces
       },
       message: (props) => `${props.value} is not a valid name!`,
     },
@@ -33,20 +33,20 @@ const personSchema = new mongoose.Schema({
     validate: {
       validator: function (value) {
         // Regex checks for 2-3 digits, a hyphen, and then at least 5 digits
-        return /^\d{2,3}-\d{5,}$/.test(value);
+        return /^\d{2,3}-\d{5,}$/.test(value)
       },
       message: (props) =>
         `${props.value} is not a valid phone number! It should be in the format XX-YYYYYY or XXX-YYYYYY.`,
     },
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
